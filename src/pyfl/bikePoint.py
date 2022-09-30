2fb67e6dc45e5716dbe7a0fef96afa68a6de0efc
Sam <sambpilot@gmail.com>
2022-09-30 08:31:47 +0100

    change return type

--- src/pyfl/bikePoint.py
+++ src/pyfl/bikePoint.py
@@ -13,7 +13,31 @@ class bike():
         Returns all the bike points
 
         :return: all the bike points in london.
-        :rtype: str
+        :rtype: dict
         """
         endpoint = f"/BikePoint/"
+        return Helper.parse(Helper.make_raw_api_call(endpoint))
+
+    def get_bike_point(self, id:int):
+        """
+        Returns a specfic bike point
+            
+        :param id: id of the bike point to get.
+        :type id: int
+        :return: the bike point.
+        :rtype: dict
+        """
+        endpoint = f"/BikePoint/{id}"
+        return Helper.parse(Helper.make_raw_api_call(endpoint))
+
+    def get_bike_point_by_name(self, name:str):
+        """
+        Returns a specfic bike point
+            
+        :param name: name of the bike point to get.
+        :type name: str
+        :return: the bike point.
+        :rtype: dict
+        """
+        endpoint = f"/BikePoint/Search?query={name}"
         return Helper.parse(Helper.make_raw_api_call(endpoint))
